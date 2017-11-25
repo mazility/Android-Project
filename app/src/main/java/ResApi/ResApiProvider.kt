ae32ee815b596aa552687acb725d813c691c1b88
mazility <pannachet.l@gmail.com>
2017-11-26 01:09:09 +0700

    Project Update

--- app/src/main/java/ResApi/ResApiProvider.kt
+++ app/src/main/java/ResApi/ResApiProvider.kt
@@ -2,18 +2,10 @@ package ResApi
 
 import android.util.Log
 import com.github.kittinunf.fuel.*
-import com.github.kittinunf.fuel.android.extension.responseJson
-import com.github.kittinunf.fuel.core.FuelError
-import com.github.kittinunf.fuel.core.FuelManager
-import com.github.kittinunf.fuel.core.Response
-import com.github.kittinunf.fuel.core.ResponseDeserializable
-import com.github.kittinunf.fuel.core.interceptors.loggingResponseInterceptor
-import com.github.kittinunf.fuel.livedata.liveDataObject
-import com.github.kittinunf.fuel.rx.rx_object
-import com.github.kittinunf.result.Result
-import com.github.kittinunf.result.getAs
 import Model.Faculty
-
+import android.os.AsyncTask
+import android.widget.ListView
+import com.example.pncht.ui.BulletinAdapter
 
 
 /**
@@ -27,24 +19,13 @@ class ResApiProvider {
     fun getListOfFaculty(): ArrayList<Faculty> {
         val listOfFaculties = ArrayList<Faculty>()
 
-       /*Fuel.get(url+"/faculties").responseJson  { request, response, result ->
-           Log.d(TAG, request.toString())
-           Log.d(TAG, response.toString())
-
-           result.fold({ data ->
-               Log.d(TAG, data.array().toString())
-               data.array().run {  }
-           }, { err ->
-               Log.e(TAG, err.toString())
-           })
-        }*/
         Fuel.get(url+"/faculties").responseObject(Faculty.Deserializer())  { request, response, result ->
             Log.d(TAG, request.toString())
             Log.d(TAG, response.toString())
 
             result.fold({ faculties ->
                 faculties?.forEach { f ->
-                   listOfFaculties.add(f)
+                    listOfFaculties.add(f)
                 }
             }, { err ->
                 Log.e(TAG, err.toString())
